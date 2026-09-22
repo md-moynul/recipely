82ccac8f07dcf502f60791d1bb5fa861423e838c
md-moynul <mdmoynulislam58995@gmail.com>
2026-09-22 20:13:17 +0600

    feat(providers): wrap CartProvider in application root providers

--- src/components/provider/Providers.jsx
+++ src/components/provider/Providers.jsx
@@ -1,10 +1,13 @@
 "use client";
 import { ThemeProvider } from "next-themes";
+import { CartProvider } from "@/context/CartContext";
 
 export function Providers({ children }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="light">
-            {children}
+            <CartProvider>
+                {children}
+            </CartProvider>
         </ThemeProvider>
     );
 }
